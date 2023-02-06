66e1094b5772852228a2fb227552c322152a623c
Nina Khmissi <samninvik@mail.ru>
2023-02-06 15:30:28 +0500

    Семинар 2. Задание 3. Рефакторинг

--- Sem2_Ex3_Ver1.py
+++ Sem2_Ex3_Ver1.py
@@ -1,5 +1,7 @@
-#Реализуйте алгоритм перемешивания списка. НЕ ИСПОЛЬЗОВАТЬ ВСТРОЕННЫЕ БИБЛИОТЕКИ SHUFFLE, максимум использование библиотеки Random для и получения случайного int
+# Реализуйте алгоритм перемешивания списка. НЕ ИСПОЛЬЗОВАТЬ ВСТРОЕННЫЕ БИБЛИОТЕКИ SHUFFLE, максимум использование библиотеки Random для и получения случайного int
 
+'''
+#первоначальный код
 from random import randint
 
 N=int(input('Введите количество элементов списка: '))
@@ -23,3 +25,36 @@ for i in range(N):
 print ('Перемешанный массив:  ', end=' ')
 for i in my_list:
     print(i, end=' ')
+'''
+from random import randint
+
+# проверка ввода данных
+def f_input(number):
+    while True:
+        try:
+            int(number)
+            break
+        except ValueError:
+            number = input('Введите ЧИСЛОВОЕ значение:')
+    return number
+
+N = int(f_input(input('Введите количество элементов списка: ')))
+start = int(f_input(input('Введите диапазон значения чисел от: ')))
+finish = int(f_input(input('\t\t\t\tдо: ')))
+
+my_list = []
+
+for i in range(N):
+    my_list.append(randint(start, finish))
+
+print('Первоначальный массив: ', end=' ')
+print(*my_list)
+
+for i in range(N):
+    time_index = randint(0, N-1)
+    b = my_list[i]
+    my_list[i] = my_list[time_index]
+    my_list[time_index] = b
+
+print('Перемешанный массив:   ', end=' ')
+print(*my_list)
