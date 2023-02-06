962c7b48e6ea0fc657b5406638fd070cd037773a
Nina Khmissi <samninvik@mail.ru>
2023-02-06 15:43:07 +0500

    Семинар 3. Задание 2. Рефакторинг

--- Sem3_Ex2.py
+++ Sem3_Ex2.py
@@ -3,6 +3,8 @@
 # [2, 3, 4, 5, 6] => [12, 15, 16];
 # [2, 3, 5, 6] => [12, 15]
 
+'''
+#первоначальный код
 from random import randint
 
 N = int(input('Введите количество элементов списка: '))
@@ -21,5 +23,33 @@ if len(my_list) % 2 == 0:
 else:
     for i in range(N//2+1):
         my_new_list.append(my_list[i]*my_list[N-1-i])
+
+print(f'{my_list} => {my_new_list}')
+'''
+from random import randint
+
+# проверка ввода данных
+def f_input(number):
+    while True:
+        try:
+            int(number)
+            break
+        except ValueError:
+            number = input('Введите ЧИСЛОВОЕ значение:')
+    return number
+
+N = int(f_input(input('Введите количество элементов списка: ')))
+start = int(f_input(input('Введите диапазон значения чисел от: ')))
+finish = int(f_input(input('\t\t\t\tдо: ')))
+
+my_list = []
+
+for i in range(N):
+    my_list.append(randint(start, finish))
+
+if len(my_list) % 2 == 0:
+    my_new_list = list((my_list[i]*my_list[N-1-i]) for i in range(N//2))
+else:
+    my_new_list = list(my_list[i]*my_list[N-1-i] for i in range(N//2+1))
 
 print(f'{my_list} => {my_new_list}')
