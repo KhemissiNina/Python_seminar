04a632916a7e4965c17cd4fc5c1d2db80c6d1738
Nina Khmissi <samninvik@mail.ru>
2023-02-06 23:54:21 +0500

    Семинар 3. Задание 3. Рефакторинг

--- Sem3_Ex3.py
+++ Sem3_Ex3.py
@@ -2,6 +2,8 @@
 # Пример:
 # [1.1, 1.2, 3.1, 5, 10.01] => 0.19
 
+'''
+#первоначальный код
 from random import uniform
 
 N = int(input('Введите количество элементов списка: '))
@@ -40,3 +42,34 @@ for i in range(N):
             print(int(my_list[i]), end=', ')
         else:
             print(my_list[i], end=', ')
+'''
+from random import uniform
+
+# проверка ввода данных
+def f_input(number):
+    while True:
+        try:
+            float(number)
+            break
+        except ValueError:
+            number = input('Введите ЧИСЛОВОЕ значение:')
+    return number
+
+N = int(f_input(input('Введите количество элементов списка: ')))
+start = float(f_input(input('Введите диапазон значения чисел от: ')))
+finish = float(f_input(input('\t\t\t\tдо: ')))
+
+my_list = []
+
+for i in range(N):
+    number = round(uniform(start, finish), 2)
+    my_list.append(number)
+
+# принудительно зададим целое число
+my_list[3] = 5
+
+change_list = list(round((my_list[i]-int(my_list[i])), 2)
+                   for i in range(len(my_list)))
+change_list = list(filter(lambda item: item > 0, change_list))
+
+print(my_list, '=>', round((max(change_list)-min(change_list)), 2))
