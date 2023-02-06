fdc817419e422cf0394cff9cf51ada0570f1680b
Nina Khmissi <samninvik@mail.ru>
2023-02-06 15:26:41 +0500

    Семинар 2. Задание 2. Рефакторинг

--- Sem2_Ex1.py
+++ Sem2_Ex1.py
@@ -1,8 +1,9 @@
-#Напишите программу, которая принимает на вход вещественное число и показывает сумму его цифр.
-#Пример:
-#6782 -> 23
-#0,56 -> 11
-
+# Напишите программу, которая принимает на вход вещественное число и показывает сумму его цифр.
+# Пример:
+# 6782 -> 23
+# 0,56 -> 11
+'''
+#первоначальный код
 s = input ('Введите число: ')
 sum=0
 for i in range(len(s)):
@@ -11,3 +12,23 @@ for i in range(len(s)):
     else:
         sum=sum+int(s[i])
 print(f'Сумма цифр числа {s} равна', sum)
+'''
+
+# проверка ввода данных
+def f_input(number):
+    while True:
+        try:
+            float(number)
+            break
+        except ValueError:
+            number = input('Введите ЧИСЛОВОЕ значение:')
+    return number
+
+
+s = s_first = f_input(input('Введите число: '))
+s = s.replace('-', '').replace('0.', '')
+
+sum = 0
+for i in range(len(s)):
+    sum = sum+int(s[i])
+print(f'Сумма цифр числа {s_first} равна', sum)
